6b3aaf52217077d1427abcaa1a715015b4477eb2
bleon133 <brot10102017@gmail.com>
2023-04-25 07:51:39 -0500

    primer commit

---
+++ taller2.py
@@ -0,0 +1,29 @@
+# Primer punto
+
+a = 40 + 2 * 7 ^ 2
+print('Punto 1 a: ', a)
+b = 3+ (2+5)*1+ (3-2*3)
+print('Punto 1 b: ',b)
+c = 16 - 8 * (10 - (5 + 4))
+print('Punto 1 c: ',c)
+d = (5+3*2)-(7*2)*(3+ (2*3-1))
+print('Punto 1 d: ',d)
+e = (24+220)/3*2
+print('Punto 1 e: ',e)
+f = (6/3*2)-(7*2)+ (2*(3+ (2/3-1)*4))
+print('Punto 1 f: ',f)
+
+# Segundo punto
+
+g = 29.7 + 5.0 ** 2.0
+print('Punto 2 a: ',g)
+h = ( (2 - 3) ** 4 * 5 / (4 + 3 * 9) )
+print('Punto 2 b: ',h)
+i = 49.38 + 127.73 - 15.02 * 6.83 / 3.22
+print('Punto 2 c: ',i)
+j = 19/3
+print('Punto 2 d: ',j)
+k = 15%6
+print('Punto 2 e: ',k)
+l = ((15 * 8 % 6 + 24 /2 ** 3) ** 3 / 4) * (5 ** 1 / 2 + 1 / 4 + 2 ** 3 - 4)
+print('Punto 2 f: ',l)
